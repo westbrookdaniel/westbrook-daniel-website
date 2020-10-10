179bbc37f4f27f6ea30a7123cdb33b35ca8d7d2f
westy12dan <westy12dan@gmail.com>
2020-10-10 18:22:05 +0800

    tweaks

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { normal } from '../util/transition'
 
-export default function Button({ children, ...props }) {
+export default function Button({ children, theme = 'yellow', className, ...props }) {
+	let themeString
+	switch (theme) {
+		case 'yellow':
+			themeString = 'bg-themeYellow-300 hover:bg-themeYellow-400 active:bg-themeYellow-500 text-black'
+			break;
+		case 'gray':
+			themeString = 'bg-themeGray-200 hover:bg-themeGray-300 active:bg-themeGray-400 text-black'
+			break;
+		default:
+			themeString = 'bg-themeYellow-300 hover:bg-themeYellow-400 active:bg-themeYellow-500 text-black'
+			break;
+	}
+	
+	let classString = `${themeString} btn ${normal} `
+	if (className) classString += `${className} `
+	
 	return (
 		<button
-			className={`btn text-black bg-yellow-600 hover:bg-yellow-700 active:bg-yellow-800 ${normal}`}
+			className={classString}
 			{...props}
 		>
 			{children}
