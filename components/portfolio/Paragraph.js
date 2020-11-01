6bd682a1890eaf6dd8e9bef77fdabe50522c610f
westy12dan <westy12dan@gmail.com>
2020-11-01 23:21:40 +0800

    portfolio page

--- components/portfolio/Paragraph.js
+++ components/portfolio/Paragraph.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export default function Paragraph({ children }) {
+export default function Paragraph({ withStack, children }) {
+	if (withStack?.length > 0) {
+		return (
+			<div className="flex items-end flex-row space-x-8 my-8 w-100">
+				<div className="flex-grow max-w-4xl">{children}</div>
+				<p className="text-right flex-1">
+					{withStack.map(item => <>{item}<br/></>)}
+				</p>
+			</div>
+		)
+
+	}
 	return <div className="max-w-4xl my-8">{children}</div>
 }
