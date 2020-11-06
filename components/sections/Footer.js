e0769fdb80a6ef34e0f5ce0870a867aa68883cd5
westy12dan <westy12dan@gmail.com>
2020-11-06 23:30:26 +0800

    updates

--- components/sections/Footer.js
+++ components/sections/Footer.js
@@ -6,13 +6,13 @@ export default function Footer() {
 			<div className="lg:flex">
 				<div className="flex flex-grow items-baseline">
 					<p>Daniel</p>
-                    <div className="flex-grow h-line mx-4 bg-themeGray-300"></div>
+					<div className="flex-grow h-line mx-4 bg-themeGray-300"></div>
 					<p>Westbrook</p>
 				</div>
-				<div className="flex mt-8 lg:mt-0 lg:ml-32 space-x-8">
-					<p>Side Projects</p>
-					<p>Resume</p>
-					<p>Graphic Design Work</p>
+				<div className="flex mt-6 lg:mt-0 lg:ml-16 space-x-8">
+					<a href="images/static/daniel-westbrook-resume.pdf" download>
+						<p>Resume</p>
+					</a>
 				</div>
 			</div>
 		</Container>
