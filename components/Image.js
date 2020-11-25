7daa9121abee432c0a1f6f004e568861bf7e776d
westy12dan <westy12dan@gmail.com>
2020-11-25 11:22:08 +0800

    isLoading for image

--- components/Image.js
+++ components/Image.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import NextImage from 'next/image'
 
 export default function Image({
@@ -6,14 +7,25 @@ export default function Image({
 	width,
 	height,
 	loading = 'lazy',
+	src,
 	...props
 }) {
+	const [isLoading, setIsLoading] = useState(true)
+
 	let classString = ''
 	if (contain) classString = 'h-full object-contain'
 	if (className) classString += ` ${className}`
+	if (isLoading) classString += ' bg-themeGray-600'
 
 	const res = width && height ? { width, height } : { unsized: true }
 	return (
-		<NextImage {...res} loading={loading} className={classString} {...props} />
+		<NextImage
+			{...res}
+			src={src}
+			onLoad={() => setIsLoading(false)}
+			loading={loading}
+			className={classString}
+			{...props}
+		/>
 	)
 }
