fa241a69081f1c1373ace3bf12bad92443c11a3a
westy12dan <westy12dan@gmail.com>
2020-11-25 11:27:13 +0800

    remove image loading

--- components/Image.js
+++ components/Image.js
@@ -10,19 +10,15 @@ export default function Image({
 	src,
 	...props
 }) {
-	const [isLoading, setIsLoading] = useState(true)
-
 	let classString = ''
 	if (contain) classString = 'h-full object-contain'
 	if (className) classString += ` ${className}`
-	if (isLoading) classString += ' bg-themeGray-600'
 
 	const res = width && height ? { width, height } : { unsized: true }
 	return (
 		<NextImage
 			{...res}
 			src={src}
-			onLoad={() => setIsLoading(false)}
 			loading={loading}
 			className={classString}
 			{...props}
