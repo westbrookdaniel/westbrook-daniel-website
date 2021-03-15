36477461bcd3831c96154e5706bf6d4ddc6a3742
westy12dan <westy12dan@gmail.com>
2021-03-15 22:20:07 +0800

    update hero

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,37 +1,25 @@
-import Container from '../layout/Container'
-import Image from '../Image'
-import styled from '@emotion/styled'
-import Link from 'next/link'
-import Nav from '../parts/Nav'
-
-const Half = styled.div`
-	height: 50%;
-`
+import Container from "../layout/Container";
+import Nav from "../parts/Nav";
+import Image from "../Image";
 
 export default function Hero() {
-
 	return (
 		<Container className="m-auto">
 			<div className="w-full pb-10">
 				<Nav className="pt-10" />
 			</div>
 			<h1 className="max-w-title">
-				Full Stack Developer and Designer in Perth Building Modernistic Digital
-				Solutions
-			</h1>
-			<Link href="/p/stories-for-reddit" scroll={false}>
-				<a>
-					<Image
-						className="cursor-pointer w-auto object-contain"
-						loading="eager"
-						width={900}
-						height={600}
-						quality={90}
-						src="/images/projects/hero-image.jpg"
-						alt="Hero Project"
-					/>
+				Full Stack Developer and Designer, Currently
+				Working at{" "}
+				<a
+					href="https://unearthed.solutions/"
+					className="hover:underline italic"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Unearthed
 				</a>
-			</Link>
+			</h1>
 		</Container>
-	)
+	);
 }
