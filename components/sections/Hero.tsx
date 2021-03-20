2f5943cbc292f4ab388c28eea0cbf245d15942ca
westy12dan <westy12dan@gmail.com>
2021-03-20 23:40:20 +0800

    upgraded hero with image

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -8,17 +8,28 @@ export default function Hero() {
             <div className="w-full pb-10">
                 <Nav className="pt-10" />
             </div>
-            <h1 className="max-w-title">
-                Full Stack Developer and Designer, Currently Working at{' '}
-                <a
-                    href="https://unearthed.solutions/"
-                    className="hover:underline italic"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Unearthed
-                </a>
-            </h1>
+            <div className="grid items-center xl:grid-cols-4 lg:grid-cols-3 gap-10 xl:gap-16">
+                <h1 className="max-w-title lg:text-5xl xl:text-6xl col-span-2 xl:col-span-3">
+                    Full Stack Developer and Designer, Currently Working at{' '}
+                    <a
+                        href="https://unearthed.solutions/"
+                        className="hover:underline italic"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Unearthed
+                    </a>
+                </h1>
+                <div className="flex justify-end items-center">
+                    <div className="w-48 max-h-48 xl:w-64 xl:max-h-64">
+                        <Image
+                            width={200}
+                            height={200}
+                            src="/images/projects/me.jpg"
+                        />
+                    </div>
+                </div>
+            </div>
         </Container>
     )
 }
