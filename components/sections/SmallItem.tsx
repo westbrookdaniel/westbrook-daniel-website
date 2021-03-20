aca5a321cdb0f1694de8ac958d739fc3fccff4b6
westy12dan <westy12dan@gmail.com>
2021-03-20 23:53:56 +0800

    update small item

--- components/sections/SmallItem.tsx
+++ components/sections/SmallItem.tsx
@@ -5,7 +5,9 @@ export default function SmallItem({ data }: { data: any }) {
         <a href={data.slug} target="_blank" rel="noopener noreferrer">
             <div className="hover-arrow">
                 <div className="flex w-full items-center">
-                    <h4 className="mr-4 text-themeGray-600">{data.title}</h4>
+                    <h4 className="font-body font-normal mr-2 text-themeGray-600">
+                        {data.title}
+                    </h4>
                     <div className="arrow">
                         <CgArrowRight color="#66767d" fontSize="1.2rem" />
                     </div>
