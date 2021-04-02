1d2a7abc5ff48a7b2fc02959642fe7838ae17d80
westy12dan <westy12dan@gmail.com>
2021-04-02 11:57:25 +0800

    add placeholder blog and project pages

---
+++ pages/blog.tsx
@@ -0,0 +1,28 @@
+import Container from '../components/layout/Container'
+import Button from '../components/Button'
+import Link from 'next/link'
+import Nav from '../components/nav/Nav'
+
+export default function Blog() {
+    return (
+        <div>
+            <Container className="m-auto">
+                <div className="w-full mb-12">
+                    <Nav className="pt-10" />
+                </div>
+                <h1 className="max-w-title">Blog</h1>
+                <Container spaced>
+                    <div className="row">
+                        <div className="max-w-xs">
+                            <Link href="/" scroll={false}>
+                                <a>
+                                    <Button>Home</Button>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </Container>
+            </Container>
+        </div>
+    )
+}
