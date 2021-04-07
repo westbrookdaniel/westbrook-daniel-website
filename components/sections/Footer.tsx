ebf5a1b36e660e82ea8ca79971cdba01ff93b064
westy12dan <westy12dan@gmail.com>
2021-04-07 19:27:58 +0800

    add footer link

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Container from '../layout/Container'
 import NavLinks from '../nav/NavLinks'
 import Socials from '../Socials'
@@ -10,11 +11,15 @@ const Footer: React.FC<Props> = ({ withSocials }) => {
     return (
         <Container spaced>
             <div className="md:flex h-full justify-between">
-                <div className="flex flex-grow items-baseline">
-                    <p>Daniel</p>
-                    <div className="flex-grow h-line mx-4 bg-themeGray-300"></div>
-                    <p>Westbrook</p>
-                </div>
+                <Link href="/" scroll={false}>
+                    <a className="flex-grow">
+                        <div className="flex items-baseline">
+                            <p>Daniel</p>
+                            <div className="flex-grow h-line mx-4 bg-themeGray-300"></div>
+                            <p>Westbrook</p>
+                        </div>
+                    </a>
+                </Link>
                 {withSocials ? <Socials className="md:ml-16" /> : null}
                 <nav className="space-x-8 mt-6 md:mt-0 md:ml-16">
                     <NavLinks />
