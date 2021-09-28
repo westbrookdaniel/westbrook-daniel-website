2e4a007ca1b877a252a0e631f5957cb007391902
westy12dan <westy12dan@gmail.com>
2021-09-28 18:59:42 +0800

    add font preloader

---
+++ pages/_document.tsx
@@ -0,0 +1,48 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+
+        return initialProps
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel="preload"
+                        href="/fonts/Rowan-Semibold.woff2"
+                        as="font"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/Rowan-SemiboldItalic.woff2"
+                        as="font"
+                        crossOrigin=""
+                    />
+                    <link
+                        rel="preload"
+                        href="/fonts/Roboto-Regular.woff2"
+                        as="font"
+                        crossOrigin=""
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
