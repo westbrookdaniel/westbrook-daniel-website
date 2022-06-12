77077915b30fe805300e282ebf2279f110e42f85
westy12dan <westy12dan@gmail.com>
2022-06-12 20:19:24 +0800

    Fix new typing

--- components/Typing.tsx
+++ components/Typing.tsx
@@ -3,30 +3,66 @@ import { repeat, useFrameSequencer, usePause } from 'sequence-man'
 import WebFont from 'webfontloader'
 import Type from './Type'
 
-const textContent = 'Daniel Westbrook'
+const logoContent = [
+    'Daniel',
+    <span key="spacer" className="ml-2" />,
+    'Westbrook',
+]
 
 function* typing() {
-    const message = textContent
-    let str = ''
-    while (str.length !== message.length) {
-        str += message[str.length]
-        yield* repeat(6, <Type>{str}</Type>)
+    const messageArr = [...logoContent]
+    const elArr = []
+    while (elArr.length !== messageArr.length) {
+        const currentItem: string | JSX.Element = messageArr[elArr.length]
+        if (typeof currentItem === 'string') {
+            let msg = ''
+            while (msg.length !== currentItem.length) {
+                msg += currentItem[msg.length]
+                yield* repeat(
+                    6,
+                    <Type>
+                        {elArr}
+                        {msg}
+                    </Type>
+                )
+            }
+            elArr.push(currentItem)
+        } else {
+            elArr.push(currentItem)
+            yield* repeat(6, <Type>{elArr}</Type>)
+        }
     }
 }
 
 function* blink() {
     for (let i = 1; i <= 6; i++) {
-        yield* repeat(45, <Type>{textContent}</Type>)
-        yield* repeat(45, <Type withoutBox>{textContent}</Type>)
+        yield* repeat(45, <Type>{logoContent}</Type>)
+        yield* repeat(45, <Type withoutBox>{logoContent}</Type>)
     }
-    yield* repeat(45, <Type>{textContent}</Type>)
+    yield* repeat(45, <Type>{logoContent}</Type>)
 }
 
 function* deleting() {
-    let str = textContent
-    while (str.length > 0) {
-        str = str.split('').slice(0, -1).join('')
-        yield* repeat(6, <Type>{str}</Type>)
+    let elArr = [...logoContent]
+    while (elArr.length > 0) {
+        const currentItem: string | JSX.Element = elArr[elArr.length - 1]
+        if (typeof currentItem === 'string') {
+            let msg = currentItem
+            elArr = elArr.slice(0, -1)
+            while (msg.length > 0) {
+                msg = msg.split('').slice(0, -1).join('')
+                yield* repeat(
+                    6,
+                    <Type>
+                        {elArr}
+                        {msg}
+                    </Type>
+                )
+            }
+        } else {
+            elArr = elArr.slice(0, -1)
+            yield* repeat(6, <Type>{elArr}</Type>)
+        }
     }
 }
 
