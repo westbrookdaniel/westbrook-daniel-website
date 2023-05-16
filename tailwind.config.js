a8c9d6add5079444f9bb79fab0f934f683cc54a8
westy12dan <westy12dan@gmail.com>
2023-05-16 23:02:24 +0800

    basic markdown styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ export default {
     theme: {
         extend: {
             colors: {
-                bg: '#514810',
-                text: '#aef4e2',
+                bg: 'rgb(var(--bg) / <alpha-value>)',
+                text: 'rgb(var(--text) / <alpha-value>)',
             },
             fontFamily: {
                 body: ['GeneralSans-Regular', 'sans-serif'],
@@ -13,6 +13,47 @@ export default {
             },
             screens: {
                 xs: '400px',
+            },
+            typography: theme => {
+                const heading = {
+                    color: theme('colors.text'),
+                    fontWeight: 'normal',
+                }
+                return {
+                    DEFAULT: {
+                        css: {
+                            color: theme('colors.text'),
+                            a: {
+                                color: theme('colors.text'),
+                                '&:hover': {
+                                    color: theme('colors.text'),
+                                },
+                            },
+                            code: {
+                                color: theme('colors.text'),
+                                fontWeight: 'normal',
+                            },
+                            'ul > li::marker': {
+                                color: theme('colors.text'),
+                            },
+                            pre: {
+                                border: '2px solid',
+                                borderColor: 'rgb(var(--text) / 0.1)',
+                                backgroundColor: 'transperant',
+                            },
+                            'pre code': {
+                                color: 'rgb(var(--text) / 1)',
+                            },
+                            h1: heading,
+                            h2: heading,
+                            h2: heading,
+                            h3: heading,
+                            h4: heading,
+                            h5: heading,
+                            h6: heading,
+                        },
+                    },
+                }
             },
         },
     },
