a6e8264d13c4b4ad8b4c6e2e5e83079ff108c316
westy12dan <westy12dan@gmail.com>
2023-05-17 01:01:10 +0800

    Fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,10 @@ import Email from '@ant-design/icons-svg/lib/asn/MailFilled'
 import { renderIconDefinitionToSVGElement } from '@ant-design/icons-svg/lib/helpers'
 
 const getHtml = (def: IconDefinition) => {
-    return renderIconDefinitionToSVGElement(def, {
+    let actualDef = def
+    // TODO: Why the server be like this
+    if ('default' in def) actualDef = def.default as IconDefinition
+    return renderIconDefinitionToSVGElement(actualDef, {
         extraSVGAttrs: {
             width: '1.2em',
             height: '1.2em',
