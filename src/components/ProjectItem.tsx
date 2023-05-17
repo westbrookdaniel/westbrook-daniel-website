3586e669d1c53087277b9bd63be3f9550afb6b3d
westy12dan <westy12dan@gmail.com>
2023-05-18 07:59:10 +0800

    Fix other project accent

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -3,7 +3,7 @@ import { Project } from '../data/projects'
 export default function ProjectItem({ project }: { project: Project }) {
     return (
         <a href={project.href} target="_blank" rel="noopener noreferrer">
-            <h3 class="font-body">{project.title}</h3>
+            <h3 class="font-body text-accent">{project.title}</h3>
         </a>
     )
 }
