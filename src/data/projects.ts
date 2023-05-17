59c395256033237f1882ada79588dbeb27f410c7
westy12dan <westy12dan@gmail.com>
2023-05-17 23:23:52 +0800

    Update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,39 +5,42 @@ export interface Project {
 
 export interface DetailedProject extends Project {
     description: string
-    screenshot: string
 }
 
 export const projects: Project[] = [
     {
-        href: 'https://github.com/westbrookdaniel/timetable-builder',
+        title: 'Dungeon Delivery',
+        href: 'https://github.com/westbrookdaniel/dungeon-delivery',
+    },
+    {
         title: 'Timetable Builder',
+        href: 'https://github.com/westbrookdaniel/timetable-builder',
+    },
+    {
+        title: 'etc.',
+        href: 'https://github.com/westbrookdaniel/',
     },
 ]
 
 export const detailedProjects: DetailedProject[] = [
     {
-        title: 'Brisk Poll',
-        description: 'Create and vote in polls quickly and easily',
-        href: 'https://github.com/westbrookdaniel/brisk-poll',
-        screenshot: '/images/projects/brisk-poll.png',
+        title: 'DocumentX',
+        description: 'Fullstack JSX Framework',
+        href: 'https://github.com/westbrookdaniel/documentx',
     },
     {
         title: 'Stories for Reddit',
         description: 'Discover and read community made short stories',
         href: 'https://github.com/westbrookdaniel/stories-for-reddit',
-        screenshot: '/images/projects/stories-for-reddit.png',
     },
     {
         title: 'Space of the Day',
         description: 'Discover astronomy pictures with a visual focus',
         href: 'https://github.com/westbrookdaniel/space-of-the-day',
-        screenshot: '/images/projects/space-of-the-day.jpg',
     },
     {
         title: 'TeaHome',
         description: 'Online based tea store for limited run teas',
         href: 'https://github.com/westbrookdaniel/teahome',
-        screenshot: '/images/projects/teahome.png',
     },
 ]
