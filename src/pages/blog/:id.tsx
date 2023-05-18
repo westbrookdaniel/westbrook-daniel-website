58e6ddb45332ee5d85da52304ab8736408a2232d
westy12dan <westy12dan@gmail.com>
2023-05-18 08:02:36 +0800

    Add all posts button

--- src/pages/blog/:id.tsx
+++ src/pages/blog/:id.tsx
@@ -28,9 +28,21 @@ export default async function BlogPost() {
                 </div>
             </div>
             <div
-                class="prose"
+                class="prose mb-16"
                 dangerouslySetInnerHTML={{ __html: post.html }}
             />
+            <a href="/blog" class="no-underline">
+                <button class="space-x-1.5">
+                    <span
+                        class="py-0.5"
+                        dangerouslySetInnerHTML={{ __html: ArrowLeft }}
+                    />
+                    <span>All Posts</span>
+                </button>
+            </a>
         </div>
     )
 }
+
+const ArrowLeft =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clip-rule="evenodd" /></svg>'
